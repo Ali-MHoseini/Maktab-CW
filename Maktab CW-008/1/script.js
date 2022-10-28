c53ca98f4e77e65791b7efe9f9573797c35a29cc
Ali.MHoseini <ali.mhoseini99@gmail.com>
2022-10-28 09:42:17 +0330

    First Commit && Solve Prob 1

---
+++ Maktab CW-008/1/script.js
@@ -0,0 +1,15 @@
+const captcha_check = (() => {
+    let actual_captcha = Math.floor(Math.random(100)*Math.pow(10, 5));
+    return {check : function (guess){
+    if(guess === this.actual_captcha){
+    console.log("You have written it correctly.");
+    } else {
+        console.log("You have written it incorrectly.");
+        }
+    }}
+})();
+
+//Do not edit below this line.
+var bot = captcha_check.actual_captcha;
+captcha_check.check(bot);
+console.log(bot);
